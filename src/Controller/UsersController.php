a95ad311aa705740da41aadba8f15a447254065a
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-02-01 08:57:23 +0900

    ログインしたらidをMemos/indexへ渡す

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -123,7 +123,7 @@ class UsersController extends AppController
             $user = $this->Auth->identify();
             if ($user) {
                 $this->Auth->setUser($user);
-                return $this->redirect($this->Auth->redirectUrl());
+                return $this->redirect(['action' => 'index', 'controller' => 'Memos', $user['id']]);
             }
             $this->Flash->error(__('Invalid username or password, try again'));
         }
